47f5980d7853dbb55de48f9b90ecdf128014f7cd
Bruno Hernandez <bruno@bridgecode.dev>
2022-08-14 11:38:46 -0500

    note preview card
    layout that will be used as view for the cards

---
+++ app/src/main/java/com/bruno/notes/adapters/NotesAdapter.kt
@@ -0,0 +1,24 @@
+package com.bruno.notes.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter() : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
+
+    inner class NotesViewHolder(view: View) :RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
